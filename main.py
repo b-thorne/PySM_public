fixed smoothing units

main.py
@@ -1,6 +1,6 @@
 import ConfigParser, os
 import pysm_synchrotron,pysm_thermaldust,pysm_cmb,pysm_spinningdust, pysm_noise, pysm_freefree
-from pysm import output, config2list, file_path, smooth_write
+from pysm import output, config2list, file_path, write_output_single
 import healpy as hp
 import numpy as np
 import argparse
@@ -25,11 +25,11 @@ if __name__ == '__main__':
         print ''.join("%s: %s \n" % item   for item in vars(out).items())
         print '-----------------------------------------------------'
         
-    sky = np.zeros(hp.nside2npix(out.nside))
+    sky = np.zeros([3,len(out.output_frequency),hp.nside2npix(out.nside)])
     print '----------------------------------------------------- \n'
 #Create synchrotron, dust, AME,  and cmb maps at output frequencies then add noise.
     if 'synchrotron' in out.components:
-        sky = pysm_synchrotron.main(parser.parse_args().config_file)
+        sky = sky + pysm_synchrotron.main(parser.parse_args().config_file)
 
     if 'thermaldust' in out.components:
         sky = sky + pysm_thermaldust.main(parser.parse_args().config_file)
@@ -43,6 +43,12 @@ if __name__ == '__main__':
     if 'cmb' in out.components:
         sky = sky + pysm_cmb.main(parser.parse_args().config_file)
 
+    if out.smoothing:
+        print 'Smoothing output maps.'
+        print '----------------------------------------------------- \n'
+        for i in xrange(len(out.output_frequency)): 
+            sky[:,i,:]=hp.smoothing(sky[:,i,:],fwhm=(np.pi/180./60.)*out.fwhm[i],verbose=False)
+
     if out.instrument_noise == True:
         sky = sky + pysm_noise.instrument_noise(parser.parse_args().config_file)
 
@@ -52,12 +58,7 @@ if __name__ == '__main__':
         out.components.append('noise')
 
     sky = np.swapaxes(sky,0,1)
-
-    if out.smoothing:
-        print 'Smoothing output maps.'
-        print '----------------------------------------------------- \n'
-
-    for i in xrange(len(out.output_frequency)): smooth_write(sky[i,...],out,Config,i)
+    for i in xrange(len(out.output_frequency)): write_output_single(sky[i,...],out,Config,i)
     
     print '-----------------------------------------------------\n'
     print 'PySM completed successfully. \n' 

pysm.py
@@ -37,9 +37,7 @@ def file_path(o,j):
     path = os.path.join(o.output_dir, fname)
     return path
 
-def smooth_write(sky_freq,o,Config,i):
-    if o.smoothing:
-        sky_freq = hp.smoothing(sky_freq,fwhm=(np.pi/180.)*o.fwhm[i],verbose=False)
+def write_output_single(sky_freq,o,Config,i):
     path = file_path(o,i)
     hp.write_map(path, hp.ud_grade(sky_freq, nside_out=o.nside), coord='G', column_units = ''.join(o.output_units), column_names = None, extra_header = config2list(Config,o,i))
 