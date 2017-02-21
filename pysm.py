f1ce4b9c3aa3bcaa156edb58b00d62fcd6aebc05
damonge <dam_phys@gmail.com>
2017-02-21 13:38:34 +0000

    added arbitrary number of components, frequency decorrelation

--- pysm.py
+++ pysm.py
@@ -108,6 +108,10 @@ class component(object):
             self.polq_em_template = read_map_wrapped(cdict['polq_em_template'],nside_out)
         if 'polu_em_template' in keys:
             self.polu_em_template = read_map_wrapped(cdict['polu_em_template'],nside_out)
+        if 'corr_len' in keys:
+            self.corr_len = float(cdict['corr_len'])
+        else :
+            self.corr_len=-1
         if 'freq_ref' in keys:
             self.freq_ref = float(cdict['freq_ref'])
         if 'pol_freq_ref' in keys:
@@ -425,3 +429,86 @@ def tprint(msg):
         if t0 is None: t0 = time.time()
         print >> sys.stderr, "%8.2f %s" % (time.time()-t0,msg)
 
+def get_decorrelation_matrices(freqs,freq_ref,corrlen) :
+    if corrlen<=0 :
+        rho_mean=np.ones([len(freqs),1])
+        rho_covar=np.zeros([len(freqs),len(freqs)])
+    else :
+        added_freq=False
+        freqtot=np.array([f for f in freqs])
+        if not(freq_ref in freqtot) :
+            freqtot=np.insert(freqtot,0,freq_ref)
+            added_freq=True
+        indref=np.where(freqtot==freq_ref)[0][0]
+
+        def invert_safe(m) :
+            mb=m.copy()
+            w_ok=False
+            while not w_ok :
+                w,v=np.linalg.eigh(mb);
+                wmin=np.min(w);
+                if wmin>0 :
+                    w_ok=True
+                else :
+                    mb+=np.diag(2*np.max([1E-14,-wmin])*np.ones(len(mb)))
+            winv=1./w;
+            return np.dot(v,np.dot(np.diag(winv),np.transpose(v)))
+
+        corrmatrix=np.exp(-0.5*((np.log(freqtot[:,None])-np.log(freqtot[None,:]))/corrlen)**2)
+        rho_inv=invert_safe(corrmatrix)
+        rho_uu=np.delete(np.delete(rho_inv,indref,axis=0),indref,axis=1);
+        rho_uu=invert_safe(rho_uu)
+        rho_inv_cu=rho_inv[:,indref]; rho_inv_cu=np.transpose(np.array([np.delete(rho_inv_cu,indref)]))
+        rho_uu_w,rho_uu_v=np.linalg.eigh(rho_uu);
+
+        rho_covar=np.dot(rho_uu_v,np.dot(np.diag(np.sqrt(np.maximum(rho_uu_w,np.zeros_like(rho_uu_w)))),
+                                         np.transpose(rho_uu_v)))
+        rho_mean=-np.dot(rho_uu,rho_inv_cu)
+
+        if not added_freq :
+            rho_covar_new=np.zeros([len(freqtot),len(freqtot)])
+            rho_mean_new=np.ones([len(freqtot),1])
+            rho_covar_new[:indref  ,:indref  ]=rho_covar[:indref,:indref]
+            rho_covar_new[indref+1:,:indref  ]=rho_covar[indref:,:indref]
+            rho_covar_new[:indref  ,indref+1:]=rho_covar[:indref,indref:]
+            rho_covar_new[indref+1:,indref+1:]=rho_covar[indref:,indref:]
+            rho_covar=rho_covar_new
+            rho_mean_new[:indref,:]=rho_mean[:indref,:]
+            rho_mean_new[indref+1:,:]=rho_mean[indref:,:]
+            rho_mean=rho_mean_new
+
+    return rho_covar,rho_mean
+
+def add_frequency_decorrelation(out,comp,maps_constrained,pol=True) :
+#The unit conversion takes care of the scaling being done in uK_RJ. After scaling we convert to whatever the output units are.
+    conv2 = convert_units(['u','K_RJ'],out.output_units,out.output_frequency)
+
+    rho_cov,rho_m=get_decorrelation_matrices(out.output_frequency,comp.freq_ref,comp.corr_len)
+    if pol :
+        rho_cov_pol,rho_m_pol=get_decorrelation_matrices(out.output_frequency,comp.pol_freq_ref,comp.corr_len)
+
+    if comp.corr_len>0 :
+        extra=np.dot(rho_cov,np.random.randn(len(out.output_frequency)))
+        if pol :
+            extra_pol=np.dot(rho_cov_pol,np.random.randn(len(out.output_frequency)))
+    else :
+        extra=np.zeros(len(out.output_frequency))
+        if pol :
+            extra_pol=np.zeros(len(out.output_frequency))
+
+    maps_out=np.zeros([len(out.output_frequency),3,len(maps_constrained[0])])
+    maps_out[:,0,:]=(rho_m+extra[:,None])*maps_constrained[None,0,:]
+    if pol :
+        maps_out[:,1,:]=(rho_m_pol+extra_pol[:,None])*maps_constrained[None,1,:]
+        maps_out[:,2,:]=(rho_m_pol+extra_pol[:,None])*maps_constrained[None,2,:]
+
+    scaling_freq=scale_freqs(comp,out,pol=False)*conv2[:,None]
+    if pol :
+        scaling_freq_pol=scale_freqs(comp,out,pol=True )*conv2[:,None]
+
+    maps_out[:,0,:]*=scaling_freq
+    if pol :
+        maps_out[:,1,:]*=scaling_freq_pol
+        maps_out[:,2,:]*=scaling_freq_pol
+
+    return maps_out
