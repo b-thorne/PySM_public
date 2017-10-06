823f78da6e3f23c45c2a16442168d9c0baf2596c
Ben Thorne <ben.thorne@physics.ox.ac.uk>
2017-10-06 09:00:52 -0400

    Revert "[WIP] Implementation of partial sky support"

--- pysm/components.py
+++ pysm/components.py
@@ -322,21 +322,6 @@ class Dust(object):
             print("Dust attribute 'Add_Decorrelation' not set.")
             sys.exit(1)
 
-    @property
-    def pixel_indices(self):
-        try:
-            return self.__pixel_indices
-        except AttributeError:
-            print("Dust attribute 'pixel_indices' not set.")
-
-    @property
-    def nside(self):
-        try:
-            return self.__nside
-        except AttributeError:
-            print("Dust attribute 'nside' not set.")
-            sys.exit(1)
-
     def signal(self):
         """Function to return the selected SED.
 
@@ -372,7 +357,7 @@ class Dust(object):
         return model
 
     @staticmethod
-    def draw_uval(seed, nside, pixel_indices=None):
+    def draw_uval(seed, nside):
         #Use Planck MBB temperature data to draw realisations of the temperature and spectral
         #index from normal distribution with mean equal to the maximum likelihood commander value,
         # and standard deviation equal to the commander std.
@@ -392,10 +377,7 @@ class Dust(object):
         #to note this for the future). We then udgrade the uval map to whatever nside is being
         #considered.Since nside is not a parameter Sky knows about we have to get it from
         #A_I, which is not ideal.
-        uval_map = hp.ud_grade(np.clip((4. + beta) * np.log10(T / np.mean(T)), -3., 5.), nside_out = nside)
-        if not pixel_indices is None:
-            uval_map = uval_map[pixel_indices]
-        return uval_map
+        return hp.ud_grade(np.clip((4. + beta) * np.log10(T / np.mean(T)), -3., 5.), nside_out = nside)
 
     @staticmethod
     def read_hd_data():
@@ -448,7 +430,7 @@ class Dust(object):
 
         #now draw the random realisation of uval if draw_uval = true
         if self.Draw_Uval:
-            self.Uval = self.draw_uval(self.Draw_Uval_Seed, self.nside)
+            self.Uval = self.draw_uval(self.Draw_Uval_Seed, hp.npix2nside(len(self.A_I)))
         elif not self.Draw_Uval:
             pass
         else:
@@ -506,19 +488,22 @@ class Dust(object):
                 scaling_I = RJ_factor * eval_HD17_I(nu_break, self.Nu_0_I)
                 scaling_P = RJ_factor * eval_HD17_P(nu_break, self.Nu_0_P)
 
-            else:
+                try:
+                    scaling_I = hp.ud_grade(scaling_I, nside_out = hp.npix2nside(len(self.A_I)))
+                    scaling_P = hp.ud_grade(scaling_P, nside_out = hp.npix2nside(len(self.A_I)))
+                except IndexError:
+                    pass
 
-                #calculate the intensity scaling from reference frequency
-                #self.Nu_0_I to frequency nu.
-                scaling_I = eval_HD17_I(nu, self.Nu_0_I)
-                scaling_P = eval_HD17_P(nu, self.Nu_0_P)
+                return np.array([scaling_I * self.A_I, scaling_P * self.A_Q, scaling_P * self.A_U])
+
+            #calculate the intensity scaling from reference frequency
+            #self.Nu_0_I to frequency nu.
+            scaling_I = eval_HD17_I(nu, self.Nu_0_I)
+            scaling_P = eval_HD17_P(nu, self.Nu_0_P)
 
             try:
-                scaling_I = hp.ud_grade(scaling_I, nside_out = self.nside)
-                scaling_P = hp.ud_grade(scaling_P, nside_out = self.nside)
-                if not self.pixel_indices is None:
-                    scaling_I = scaling_I[self.pixel_indices]
-                    scaling_P = scaling_P[self.pixel_indices]
+                scaling_I = hp.ud_grade(scaling_I, nside_out = hp.npix2nside(len(self.A_I)))
+                scaling_P = hp.ud_grade(scaling_P, nside_out = hp.npix2nside(len(self.A_I)))
             except IndexError:
                 pass
             return np.array([scaling_I * self.A_I, scaling_P * self.A_Q, scaling_P * self.A_U])
@@ -876,13 +861,6 @@ class CMB(object):
         except AttributeError:
             print("CMB attribute 'A_U' not set.")
 
-    @property
-    def pixel_indices(self):
-        try:
-            return self.__pixel_indices
-        except AttributeError:
-            print("CMB attribute 'pixel_indices' not set.")
-
     def signal(self):
         """Function to return the selected SED.
 
@@ -953,11 +931,7 @@ class CMB(object):
 
         @FloatOrArray
         def model(nu, **kwargs):
-            cmb_map = np.array(rm) * convert_units("uK_CMB", "uK_RJ", nu)
-            if self.pixel_indices is None:
-                return cmb_map
-            else:
-                return cmb_map[:, self.pixel_indices]
+            return np.array(rm) * convert_units("uK_CMB", "uK_RJ", nu)
         return model
 
     def synfast(self):
@@ -986,12 +960,7 @@ class CMB(object):
 
         @FloatOrArray
         def model(nu, **kwargs):
-            cmb_map = np.array([T, Q, U]) * convert_units("uK_CMB", "uK_RJ", nu)
-            if self.pixel_indices is None:
-                return cmb_map
-            else:
-                return cmb_map[:, self.pixel_indices]
-
+            return np.array([T, Q, U]) * convert_units("uK_CMB", "uK_RJ", nu)
         return model
 
     def pre_computed(self):
--- pysm/pysm.py
+++ pysm/pysm.py
@@ -303,14 +303,7 @@ class Instrument(object):
         except AttributeError:
             print("Instrument attribute 'Output_Units not set.'")
             
-    @property
-    def pixel_indices(self):
-        try:
-            return self.__pixel_indices
-        except AttributeError:
-            print("Instrument attribute 'pixel_indices' not set.")
-
-    def observe(self, Sky, write_outputs=True):
+    def observe(self, Sky):
         """Evaluate and add instrument effects to Sky's signal function.
 
         This method evaluates the Sky class's signal method at the
@@ -329,10 +322,7 @@ class Instrument(object):
         output = self.smoother(output)
         noise = self.noiser()
         output, noise = self.unit_converter(output, noise)
-        if write_outputs:
-            self.writer(output, noise)
-        else:
-            return output, noise
+        self.writer(output, noise)
         return 
         
     def apply_bandpass(self, signal, Sky):
@@ -401,16 +391,12 @@ class Instrument(object):
         :return: map plus noise, and noise -- numpy.ndarray
 
         """
-        try:
-            npix = len(self.pixel_indices)
-        except TypeError:
-            npix = hp.nside2npix(self.Nside)
-
+        npix = hp.nside2npix(self.Nside)
         if not self.Add_Noise:
             return np.zeros((len(self.Sens_I), 3, npix))
         elif self.Add_Noise:
             # solid angle per pixel in amin2
-            pix_amin2 = 4. * np.pi / float(hp.nside2npix(self.Nside)) * (180. * 60. / np.pi) ** 2
+            pix_amin2 = 4. * np.pi / float(npix) * (180. * 60. / np.pi) ** 2
             """sigma_pix_I/P is std of noise per pixel. It is an array of length
             equal to the number of input maps."""
             sigma_pix_I = np.sqrt(self.Sens_I ** 2 / pix_amin2)
@@ -476,19 +462,19 @@ class Instrument(object):
                 for f, o, n in zip(self.Frequencies, output, noise):
                     print(np.std(n, axis = 1))# * np.sqrt(4. * np.pi / float(hp.nside2npix(128)) * (180. * 60. / np.pi) ** 2)
                     print(np.std(o, axis = 1))
-                    hp.write_map(self.file_path(f = f, extra_info = "noise"), n, overwrite=True)
-                    hp.write_map(self.file_path(f = f, extra_info = "total"), o + n, overwrite=True)
+                    hp.write_map(self.file_path(f = f, extra_info = "noise"), n)
+                    hp.write_map(self.file_path(f = f, extra_info = "total"), o + n)
             elif not self.Add_Noise:
                 for f, o in zip(self.Frequencies, output):
-                    hp.write_map(self.file_path(f = f, extra_info = "total"), o, overwrite=True)
+                    hp.write_map(self.file_path(f = f, extra_info = "total"), o)
         elif self.Use_Bandpass:
             if self.Add_Noise:
                 for c, o, n in zip(self.Channel_Names, output, noise):
-                    hp.write_map(self.file_path(channel_name = c, extra_info = "total"), o + n, overwrite=True)
-                    hp.write_map(self.file_path(channel_name = c, extra_info = "noise"), n, overwrite=True)
+                    hp.write_map(self.file_path(channel_name = c, extra_info = "total"), o + n)
+                    hp.write_map(self.file_path(channel_name = c, extra_info = "noise"), n)
             elif not self.Add_Noise:
                 for c, o in zip(self.Channel_Names, output):
-                    hp.write_map(self.file_path(channel_name = c, extra_info = "total"), o, overwrite=True)
+                    hp.write_map(self.file_path(channel_name = c, extra_info = "total"), o)
         return
 
     def print_info(self):
@@ -596,7 +582,7 @@ def initialise_hd_dust_model_bandpass(hd_unint_signal, **kwargs):
 
     """
     #Draw map of uval using Commander dust data.
-    uval = Dust.draw_uval(kwargs['draw_uval_seed'], kwargs['nside'], kwargs.get('pixel_indices'))
+    uval = Dust.draw_uval(kwargs['draw_uval_seed'], hp.npix2nside(len(kwargs['A_I'])))
 
     #Read in the precomputed dust emission spectra as a function of lambda and U.
     data_sil, data_silfe, data_car, wav, uvec = Dust.read_hd_data()
