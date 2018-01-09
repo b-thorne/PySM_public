01608c08fcae41fe5c033a14ae9556117f8f7098
Andrea Zonca <code@andreazonca.com>
2018-01-09 10:46:01 -0800

    build sky config function

--- test/test_mpi.py
+++ test/test_mpi.py
@@ -2,19 +2,22 @@
 
     mpirun -np 4 python test_mpi.py
 """
+
 import numpy as np
 import healpy as hp
+
 import pysm
+from pysm.nominal import models
 from mpi4py import MPI
+
 
 def is_power2(num):
     return num != 0 and ((num & (num - 1)) == 0)
 
+
 comm = MPI.COMM_WORLD
 
-assert is_power2(comm.size), "Test with a number of MPI processes which is power of 2"
-
-from pysm.nominal import models
+assert is_power2(comm.size), "Run with a number of MPI processes which is power of 2"
 
 nside = 64
 npix = hp.nside2npix(nside)
@@ -25,32 +28,49 @@ if comm.size == 1:
     pixel_indices = None
     comm = None
 else:
-    pixel_indices = np.arange(comm.rank*num_local_pixels, (comm.rank+1)*num_local_pixels,dtype=np.int)
+    pixel_indices = np.arange(comm.rank     * num_local_pixels,
+                              (comm.rank+1) * num_local_pixels,
+                              dtype=np.int)
 
-sky_config = {
-    'synchrotron' : models("s3", nside, pixel_indices=pixel_indices, mpi_comm=comm),
-    'dust' : models("d7", nside, pixel_indices=pixel_indices, mpi_comm=comm),
-    'freefree' : models("f1", nside, pixel_indices=pixel_indices, mpi_comm=comm),
-    'cmb' : models("c1", nside, pixel_indices=pixel_indices, mpi_comm=comm),
-    'ame' : models("a2", nside, pixel_indices=pixel_indices, mpi_comm=comm),
-}
+pysm_model = "s3,d7,f1,c1,a2"
 
+def build_sky_config(pysm_model, nside, pixel_indices=None, mpi_comm=None):
+    """Build a PySM sky configuration dict from a model string"""
+
+    sky_components = [
+        'synchrotron',
+        'dust',
+        'freefree',
+        'cmb',
+        'ame',
+    ]
+
+    sky_config = dict()
+    for component_model in pysm_model.split(','):
+        full_component_name = [
+            each for each in sky_components
+            if each.startswith(component_model[0])][0]
+        sky_config[full_component_name] = \
+            models(component_model, nside=nside, pixel_indices=pixel_indices, mpi_comm=comm)
+    return sky_config
+
+sky_config = build_sky_config(pysm_model, nside, pixel_indices, comm)
 
 sky = pysm.Sky(sky_config, mpi_comm=comm)
 
 
 instrument_bpass = {
-    'use_smoothing' : False,
-    'nside' : nside,
-    'add_noise' : False,
-    'use_bandpass' : True,
-    'channels' : [(np.linspace(20, 25, 10), np.ones(10))],
-    'channel_names' : ['channel_1'],
-    'output_units' : 'uK_RJ',
-    'output_directory' : './',
-    'output_prefix' : 'test',
-    'noise_seed' : 1234,
-    'pixel_indices' : pixel_indices
+    'use_smoothing': False,
+    'nside': nside,
+    'add_noise': False,
+    'use_bandpass': True,
+    'channels': [(np.linspace(20, 25, 10), np.ones(10))],
+    'channel_names': ['channel_1'],
+    'output_units': 'uK_RJ',
+    'output_directory': './',
+    'output_prefix': 'test',
+    'noise_seed': 1234,
+    'pixel_indices': pixel_indices
 }
 
 instrument = pysm.Instrument(instrument_bpass)
@@ -58,16 +78,7 @@ local_map = instrument.observe(sky, write_outputs=False)
 
 # Run PySM again locally on each process on the full map
 
-comm = None
-
-sky_config = {
-    'synchrotron' : models("s3", nside, pixel_indices=None, mpi_comm=comm),
-    'dust' : models("d7", nside, pixel_indices=None, mpi_comm=comm),
-    'freefree' : models("f1", nside, pixel_indices=None, mpi_comm=comm),
-    'cmb' : models("c1", nside, pixel_indices=None, mpi_comm=comm),
-    'ame' : models("a2", nside, pixel_indices=None, mpi_comm=comm),
-}
-
+sky_config = build_sky_config(pysm_model, nside)
 
 sky = pysm.Sky(sky_config, mpi_comm=comm)
 
