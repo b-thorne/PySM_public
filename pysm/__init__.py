ac9997a04dbc35d3039844a90b9b70a35703e3a1
Yu Feng <rainwoodman@gmail.com>
2018-01-19 11:59:35 -0800

    Fetch template dir from PYSM root package.
    templates looked like an asset of PYSM, so the path shall be calculated in the root package.

--- pysm/__init__.py
+++ pysm/__init__.py
@@ -1,3 +1,7 @@
 from .pysm import Sky, Instrument
 from .components import Dust, Synchrotron, AME, Freefree, CMB
 from .common import read_map, convert_units
+
+def get_template_dir():
+    import os.path
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), 'template')
