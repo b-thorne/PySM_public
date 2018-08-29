dc008a028fc4456c2e5f1ee479bf9a4f8170f1de
ben thorne <ben.thorne@physics.ox.ac.uk>
2018-08-29 10:27:03 -0400

    updates

--- pysm/__init__.py
+++ pysm/__init__.py
@@ -1,3 +1,4 @@
 from .component_models import ModifiedBlackBody
 from .sky import Sky
 from .presets import preset_models
+from .units import *
---
+++ tests/test_mpi_read.py
@@ -0,0 +1,9 @@
+from runtests import MPITest
+import sys
+sys.path.append('/home/ben/Projects/PySM/PySM_public')
+import unittest
+import pysm.component_models.template import read_map
+
+
+def test_mpi_read_map():
+    return
